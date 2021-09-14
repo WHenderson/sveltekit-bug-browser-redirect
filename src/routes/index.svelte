26e26889fa43f9c529abf73bc10679cc6a3447d8
Will <will.henderso.n@gmail.com>
2021-09-14 18:35:57 +1000

    typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,5 +13,5 @@
         return {};
     }
 </script>
-<h1>Woops</h1>
-<p>You should be here. You should have been directed to /redirect!</p>
+<h1>Woops!</h1>
+<p>You shouldn't be here. You should have been directed to /redirect!</p>
